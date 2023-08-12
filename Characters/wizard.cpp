4b8931731c312d91fd3b9a38801f06f7dc610056
nebingo <ogniben.michele@gmail.com>
2023-08-12 18:48:15 +0200

    Fix Typo in wizard.cpp

--- Characters/wizard.cpp
+++ Characters/wizard.cpp
@@ -5,9 +5,9 @@ Wizard::Wizard() : Character(10, 3, 10, 5, 7, 75) { };
 // L'abilità dello stregone consiste nel ridurre del 30% ogni statistica dell'avversario (esclusa la vita)
 void Wizard::useAbility(Character* enemy)
 {
-    enemy->setMagAtk(enemy->getMagAtk() + 0.7);
-    enemy->setPhyAtk(enemy->getPhyAtk() + 0.7);
-    enemy->setMagDef(enemy->getMagDef() + 0.7);
-    enemy->setPhyDef(enemy->getPhyDef() + 0.7);
-    enemy->setSpeed(enemy->getSpeed() + 0.7);
+    enemy->setMagAtk(enemy->getMagAtk() * 0.7);
+    enemy->setPhyAtk(enemy->getPhyAtk() * 0.7);
+    enemy->setMagDef(enemy->getMagDef() * 0.7);
+    enemy->setPhyDef(enemy->getPhyDef() * 0.7);
+    enemy->setSpeed(enemy->getSpeed() * 0.7);
 }
