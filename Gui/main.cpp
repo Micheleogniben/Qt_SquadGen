478d037ad1d2d612e8fb921658dc957dbf3f2999
Lorenzopensource <72146782+Lorenzopensource@users.noreply.github.com>
2023-08-17 17:02:01 +0200

    Add files via upload

---
+++ Gui/main.cpp
@@ -0,0 +1,10 @@
+#include <QApplication>
+#include <QWidget>
+#include "gui.h"
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    Gui gui;
+    app.setWindowIcon(QIcon(":/Resources/logo.png"));
+    return app.exec();
+}
