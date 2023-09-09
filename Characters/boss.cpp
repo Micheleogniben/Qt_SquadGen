f9fc368533526f00ea176828631602348ca7bb9a
Lorenzopensource <72146782+Lorenzopensource@users.noreply.github.com>
2023-09-09 18:38:18 +0200

    Add files via upload

--- Characters/boss.cpp
+++ Characters/boss.cpp
@@ -4,7 +4,7 @@ Boss::Boss(QString n) : Character(bossStats,n) { };
 
 bool Boss::useAbility(Character* target){
     if(getAbilityUsed()) return false;
-    target->setPhyAtk(3);
+    target->setPhyAtk(1);
     setAbilityUsed(true);
     return true;
 }
